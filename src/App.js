2bc26c95b4cd469bcec68d2aa7d0277da20a1404
Lucas Carvalho <drigerm@hotmail.com>
2023-05-30 11:24:58 -0300

    colocando os botoes do opção

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Options from './components/Options';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Options />
     </div>
   );
 }
---
+++ src/components/Options.jsx
@@ -0,0 +1,20 @@
+function Options() {
+    return (
+      <div className="Options-container">
+        <button>
+          2 minutes
+        </button>
+        <button>
+          5 minutes
+        </button>
+        <button>
+          10 minutes
+        </button>
+        <button>
+          15 minutes
+        </button>
+      </div>
+    );
+  }
+
+  export default Options
